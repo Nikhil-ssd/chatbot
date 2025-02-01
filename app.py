a6e7d52fd6c61536ab2b151b4fea6f5fe4bb9d4f
Nikhil Deshpande <nikhilsd1995@gmail.com>
2025-02-01 15:38:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def chat():
     return jsonify({"response": result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
 # In[ ]:
